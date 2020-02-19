87712e524c80796e37a915cb07a4e1ecad3a10a5
Jason Watson <jason.jw@live.co.uk>
2020-02-19 08:43:18 +0100

    Add calibration and constants libraries

---
+++ constants/pybind/module.cc
@@ -0,0 +1,16 @@
+// Copyright 2020 Cherenkov Telescope Array Observatory
+// This software is distributed under the terms of the BSD-3-Clause license.
+
+#include <pybind11/pybind11.h>
+
+namespace sstcam {
+namespace constants {
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(sstcam_constants, m) {
+
+}
+
+}  // namespace constants
+}  // namespace sstcam
