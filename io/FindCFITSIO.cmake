01a02126899f7126895a0e765237590c9d760f36
Jason Watson <jason.jw@live.co.uk>
2020-02-20 13:47:40 +0100

    Fix FinsCFITSIO to correctly handle cfitsio/fitsio.h (yum install version)

--- io/FindCFITSIO.cmake
+++ io/FindCFITSIO.cmake
@@ -1,17 +1,8 @@
 # Find fitsio.h and cfitsio library
-# Ref. http://www.cmake.org/Wiki/CMake:How_To_Find_Libraries
+# Ref. https://gitlab.kitware.com/cmake/community/-/wikis/doc/tutorials/How-To-Find-Libraries
 
-find_path(CFITSIO_INCLUDE_DIR fitsio.h)
-
-# For the default yum configuration on CentOS 7
-if(CFITSIO_INCLUDE_DIR STREQUAL "CFITSIO_INCLUDE_DIR-NOTFOUND")
-  find_path(CFITSIO_INCLUDE_DIR cfitsio/fitsio.h)
-  if(NOT CFITSIO_INCLUDE_DIR STREQUAL "CFITSIO_INCLUDE_DIR-NOTFOUND")
-    set(CFITSIO_INCLUDE_DIR "${CFITSIO_INCLUDE_DIR}/cfitsio")
-  endif()
-endif()
-
-find_library(CFITSIO_LIBRARY cfitsio)
+find_path(CFITSIO_INCLUDE_DIR fitsio.h PATH_SUFFIXES cfitsio)
+find_library(CFITSIO_LIBRARY cfitsio PATH_SUFFIXES cfitsio)
 set(CFITSIO_INCLUDE_DIRS ${CFITSIO_INCLUDE_DIR})
 set(CFITSIO_LIBRARIES ${CFITSIO_LIBRARY})
 include(FindPackageHandleStandardArgs)
